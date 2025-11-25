b255cae01b859ca808eac1ee325a293334733ba5
love1roha <gjhQAQ@mail.nwpu.edu.cn>
2025-11-25 15:42:15 +0800

    输出钙离子浓度, 并统计时间

--- main.cpp
+++ main.cpp
@@ -100,7 +100,7 @@ int main()
         double dt_ode_ms = 0.005;                     // 50 microseconds = 0.05 ms
         
         // Total simulation time
-        double T_total_seconds = 0.6;                // 600 ms total
+        double T_total_seconds = 1.0;                // 1000 ms total
         
         // Calculate number of PDE steps (not ODE steps!)
         size_t num_pde_steps = static_cast<size_t>(T_total_seconds / dt_pde_seconds);
